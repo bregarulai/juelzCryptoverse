8b8a18a3226dd26e2ff9645420720f6e44a87732
bregarulai <bregarulai@gmail.com>
2021-11-27 13:47:04 -0500

    created main structure for app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Hello App
+  return <div className="app">
+    <div className="navbar">
+
     </div>
-  );
+    <div className="main">
+      
+      <div className="footer">
+
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
