d9a365465f8416ba728e7d41954e2a4a60a717c0
bregarulai <bregarulai@gmail.com>
2021-11-28 00:37:50 -0500

    created news page card

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,6 +4,9 @@ import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
+const demoImage =
+  'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 const { Text, Title } = Typography;
 const { Option } = Select;
 
@@ -13,7 +16,52 @@ const News = ({ simplified }) => {
     count: simplified ? 6 : 12,
   });
 
-  return <div>News</div>;
+  if (isFetching) return 'Loading...';
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews?.value?.map((news, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target='_blank' rel='noopener noreferrer'>
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  style={{ maxWidth: '200px', maxHeight: '100px' }}
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt={news.name}
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className='provider-container'>
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt={news.name}
+                  />
+                  <Text className='provider-name'>
+                    {news.provider[0]?.name}
+                  </Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf('ss').fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
