f6f2d2b0b4efdd82c026e14ba9bc0edcbc607db1
bregarulai <bregarulai@gmail.com>
2021-11-30 21:07:03 -0500

    created usegetexchangesquery

--- src/pages/Exchanges.jsx
+++ src/pages/Exchanges.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useGetExchangesQuery } from '../services/cryptoApi';
 
 const Exchanges = () => {
+  const { data, isFetching } = useGetExchangesQuery();
+
+  if (isFetching) return 'Loading...';
+
   return <div>Exchanges</div>;
 };
 
